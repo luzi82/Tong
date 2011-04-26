5cde32074d8bb9499430ca1db11dcd043a066b95
luzi82 <luzi82@gmail.com>
2011-04-26 20:29:55 +0800

    change typekey to be string array

--- src/tong_element/TongStruct.py
+++ src/tong_element/TongStruct.py
@@ -1,10 +1,10 @@
 from tong_element.TongElement import TongElement
-from tong_element.TongType import TongType, TongTypeKey
+from tong_element.TongType import TongType
 
 class TongStruct(TongElement, TongType):
     pass
 
 class TongStructMember(TongElement):
     def __init__(self):
-        self.typeKey = TongTypeKey()
+        self.typeKey = None
         self.dimension = 0
